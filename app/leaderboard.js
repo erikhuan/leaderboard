e78985bf7823b826b1bc71cc31a40e019d9a10b8
erikhuan <huangerik@hotmail.com>
2017-02-28 12:45:56 -1000

    Part way into Task 5

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,3 +1,23 @@
-/**
- * Created by huang on 2/28/2017.
- */
+PlayersList = new Mongo.Collection('players');
+
+if(Meteor.isClient){
+  console.log("Hello client");
+  Template.leaderboard.helpers({
+    'player': function() {
+      return PlayersList.find();
+    },
+    'selectedClass': function(){
+      return "selected";
+    },
+  });
+  Template.leaderboard.events({
+    'click .player': function(){
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    }
+  });
+}
+
+if(Meteor.isServer){
+  console.log("Hello server");
+}
